fc07b28bf9793876059b3e09f33ac80e0621a470
Dominic Martinez <dom@dominicm.dev>
2022-10-24 10:58:00 -0400

    fix(scheme): fix rebase error

--- modules/lang/scheme/packages.el
+++ modules/lang/scheme/packages.el
@@ -19,7 +19,7 @@
              (not (modulep! :checkers syntax +flymake)))
       (package! flycheck-guile
         :recipe (:host github :repo "flatwhatson/flycheck-guile")
-        :pin "e58ceb8b511cd395b9be69f4a1ff85305fbb51c3"))
+        :pin "e58ceb8b511cd395b9be69f4a1ff85305fbb51c3")))
   (when (modulep! +kawa)
     (package! geiser-kawa :pin "5896b19642923f74f718eb68d447560b2d26d797"))
   (when (modulep! +mit)
